67d74bdd20ec712cf601e049d2ba47677af4b540
Jack Leigh <jack.leigh@gmail.com>
2017-01-31 14:47:54 +0000

    Start with some types

---
+++ src/Botkit/Slack/Types.purs
@@ -0,0 +1,15 @@
+module Botkit.Slack.Types
+  ( BOTKIT
+  , RawBot
+  , RawController
+  , RawMessage
+  ) where
+
+foreign import data BOTKIT :: !
+
+foreign import data RawBot :: *
+foreign import data RawController :: *
+foreign import data RawMessage :: *
+
+foreign import data RawReply :: *
+foreign import data RawAttachment :: *
